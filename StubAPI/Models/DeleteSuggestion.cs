398487e4c5fb1e576fd1b73fa2aa708c5c20b379
gwalvanshi <gwalvanshi@gmail.com>
2018-07-03 22:39:40 +0530

    added new file

---
+++ StubAPI/Models/DeleteSuggestion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StubAPI.Models
+{
+    public class DeleteSuggestion
+    {
+        public string reasonForChange { get; set; }
+
+        public int sugId { get; set; }
+
+    }
+}
